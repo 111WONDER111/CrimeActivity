70b81d37c881bbcd56fe47417494afcea9475a97
Wonder_private <1147983691@qq.com>
2019-06-25 15:09:11 +0800

    第八章 按钮和时间记录“

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,11 +1,34 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
+/**
+ * 项目的获取和设置方法Crime类
+ */
 public class Crime {
 
     private UUID mId; // 通用唯一标识码
     private String mTitle;
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() { // 布尔型，is
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+
+    private Date mDate;// p132 第八章
+    private boolean mSolved;
 
     public UUID getmId() {
         return mId;
@@ -21,6 +44,7 @@ public class Crime {
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -7,6 +7,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
@@ -14,6 +17,8 @@ import androidx.fragment.app.Fragment;
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,19 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getmDate().toString());
+        mDateButton.setEnabled(false);// p136禁用按钮，为了以后更改按钮时期显示
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);// p136设置监听更新Crime时的mSolved变化
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setmSolved(isChecked);
+            }
+        });
+
         return v;
     }
 
